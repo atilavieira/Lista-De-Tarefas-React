3a4b0e2b653c5ef402c71b23f846ebea97c912ca
Átila vieira <80175779+atilavieira@users.noreply.github.com>
2024-10-14 22:39:11 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [todos, setTodos] = useState([
     {
       id: 1,
-      text: "criar funcionalidade x no sistema",
+      text: "criar funcionalidade ",
       category: "Trabalho",
       isCompleted: false,
     },
